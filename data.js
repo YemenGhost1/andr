24ca05b660ff6ed1fc4caa43f8c7b3cce6cdc033
YemenGhost <a4yemenza@gmail.com>
2025-07-12 14:36:33 +0300

    data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "7912402132:AAGpe9dTaYLONp5Jx73aQvtC0zjChU9elcc",
-  id:"6919638073",
-  address:"http://rat-android-7dnf.onrender.com"
-};
+  botToken: process.env.BOT_TOKEN,
+  chatId: process.env.CHAT_ID,
+  domain: process.env.DOMAIN
+}
